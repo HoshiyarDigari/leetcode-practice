1f4c58cc282782a14789ff20afbc302599a09849
HoshiyarDigari <hoshiyarsingh@gmail.com>
2025-01-22 14:14:31 -0500

    completed isSubsequence, got 100%, only slight improvement to stop inner loop

--- isSubsequence.py
+++ isSubsequence.py
@@ -4,8 +4,32 @@ def isSubsequence(s, t):
      :type t: str
      :rtype: bool
      """
+     # pointers to keep track of location in the two strings
+     s_index = 0
+     t_index = 0
+     print(s,t)
+     # start with first element of s 
+     if not s:
+         return True
+     while s_index <= len(s) -1 and t_index < len(t):
+        print('checking for ', s[s_index])
+        # compare with elements in t to find this character of s
+        while t_index < len(t):
+            print('comparing t element ', t[t_index], 'against s element', s[s_index])
+            if t[t_index] == s[s_index]:
+                s_index+=1
+            t_index+=1
+        if s_index== len(s):
+            print(True)
+            return True
+     return False
+        
+        
+
+          
           
 
 if __name__ == "__main__":
-    s = ["a","a","b","b","b","b","b","b","b","b","b","b","b","b", "c", "c", "c"]
-    compress(s)
+    t = "ahbgdc"
+    s = ""
+    isSubsequence(s,t)
