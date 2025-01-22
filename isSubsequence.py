7855abbfb16c7c0205c4c7450ffcee7add8a740d
HoshiyarDigari <hoshiyarsingh@gmail.com>
2025-01-22 11:58:55 -0500

    starting with this problem

---
+++ isSubsequence.py
@@ -0,0 +1,11 @@
+def isSubsequence(s, t):
+     """
+     :type s: str
+     :type t: str
+     :rtype: bool
+     """
+          
+
+if __name__ == "__main__":
+    s = ["a","a","b","b","b","b","b","b","b","b","b","b","b","b", "c", "c", "c"]
+    compress(s)
