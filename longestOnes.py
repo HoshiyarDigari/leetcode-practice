0a0966864b1e48f1263575281aa7dea9f0963279
HoshiyarDigari <hoshiyarsingh@gmail.com>
2025-01-28 13:29:08 -0500

    longest ones solved with chatgpt help, my solution was good but time exceeded. dont use list slicing, its expensive operation

--- longestOnes.py
+++ longestOnes.py
@@ -1,32 +1,40 @@
-def longestOnes(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
+def longestOnes(nums, k):
+     """
+     :type nums: List[int]
+     :type k: int
+     :rtype: int
 
-        algo:
+     algo:
+     we can flip atmost k 0's, so we can have a window that has at most k 0's and rest of them are 1's. This is our initial window as we scan from left to right.
+     then we move to next window and add or subtract 1 as needed account for incoming on right and outgoing on left. If our k constraint is good, we increase this window size.
+     if exceeded, we slide the window frame to the right again keeping track of biggest window we have seen so far.
+     """
+     zero_count = 0
+     left = 0
+     right = 0
+     max_ones =0
+     #get first window of maximum ones
+     print(nums)
+     for right in range(len(nums)):
+          print('looking at ', nums[left:right+1])
+          if nums[right] == 0:
+               zero_count+=1
+          # right is increased by the for loop , so we don't write an else statement
+          while zero_count > k:
+               if nums[left] == 0:
+                    zero_count-=1
+               left+=1
+          print('(', left, ',', right, ') count of zero is ', zero_count)
+          max_ones = max(max_ones, right -left +1)
+          print(max_ones)
+     print(max_ones)
+     return max_ones     
+
+
+          
+          
+              
         
-        """
-        left = 0 
-        right = k - 1
-        vowels = {'a', 'e', 'i', 'o', 'u'}
-        max_vowels = 0
-
-        # 1st window
-        for i in range(left, right + 1):
-            if s[i] in vowels:
-                max_vowels += 1
-
-        window_vowels = max_vowels
-
-        # Sliding window with a for loop
-        for right in range(k, len(s)):
-            window_vowels += (s[right] in vowels) - (s[right - k] in vowels)
-            if window_vowels > max_vowels:
-                max_vowels = window_vowels
-        print(max_vowels)
-        return max_vowels
-            
               
               
 
@@ -36,6 +44,6 @@ def longestOnes(self, nums, k):
 
 
 if __name__ == "__main__":
-    s="ppuuoibwiogewxvgxvugwatxebstgdciswnkynvsbktfrrzxddzpjzvmqnjigkyviwoniyfkhadnqgihbobjjgcmmmnqzsckkqnfonepbgvzrnwxlrdawkwwbnwfouofdtskznfglbsfdaulxj"
-    k = 20
-    maxVowels(s, k)
+    nums = [0]
+    k = 0
+    longestOnes(nums, k)
