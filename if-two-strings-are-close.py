e14c7207e5a3cd8703210b5040cddaa0e8862348
HoshiyarDigari <hoshiyarsingh@gmail.com>
2025-01-31 11:29:28 -0500

    solved equal pairs, improved runtime on closeStrings using tuple instead of list

--- if-two-strings-are-close.py
+++ if-two-strings-are-close.py
@@ -1,4 +1,5 @@
-def closeStrings(word1, word2):
+
+def closeStrings( word1, word2):
     """
     :type word1: str
     :type word2: str
@@ -15,20 +16,36 @@ def closeStrings(word1, word2):
     frequencies2 ={}
     for char in word2:
         frequencies2[char]=frequencies2.get(char,0)+1
-    ##print(frequencies1,'\n', frequencies2)
+    print(frequencies1,'\n', frequencies2)
 
     #compare the dictionaries
-    ##print(set(frequencies1.keys()), set(frequencies2.keys()))
-    ##print((frequencies1.values()), (frequencies2.values()))
+    print(set(frequencies1.keys()), set(frequencies2.keys()))
+    print((frequencies1.values()), (frequencies2.values()))
     if frequencies1 == frequencies2:
-        #print(True)
+        print(True)
         return True
     # compare keys and values
     if set(frequencies1.keys()) == set(frequencies2.keys()):
-       return sorted(frequencies1.values()) == sorted(frequencies2.values())
-    #print(False)
+        values2 = tuple(frequencies2.values())
+        print('values2', values2)
+        for number in frequencies1.values():
+            print(number, values2)
+            if number not in values2:
+                print(False)
+                return False
+            else:
+                index = values2.index(number)
+                print('deleting', index, values2, number)
+                values2 = values2[:index] + values2[index+1:]
+                print('new values2', values2)
+            
+        print(True)
+        return True
+    print(False)
     return False
+
+
 if __name__ == "__main__":
     word1 = "cabbba"
-    word2 = "aabbss"
+    word2 = "abbccc"
     closeStrings(word1, word2)
