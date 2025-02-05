b2ea927ee781d0f3c349c9df84b934b936116b0b
HoshiyarDigari <hoshiyarsingh@gmail.com>
2025-02-05 13:53:29 -0500

    solved  but with low score

--- predictPartyVictory.py
+++ predictPartyVictory.py
@@ -2,15 +2,20 @@ from collections import deque
 def predictPartyVictory(senate: str) -> str:
     queue = deque(senate)
     complement = {'R':'D', 'D':'R'}
-    counter = 0
-    while queue and counter < 7:
-        counter +=1
+
+    while queue:
+       
         for senator in queue:
+            # this senator's best plan is to remove senator from other party
             if complement[senator] in queue:
                 queue.remove(complement[senator])
-                print(queue)
+                #print(queue)
+                # move this senator to the end of queue as his turn will now come in the next round
+                queue.rotate(-1)
+                #print(queue)
                 break
             else:
+                # if there are no complements then we only have one party's senators.
                 print('winner is ', queue[0])
                 return queue[0]
     
