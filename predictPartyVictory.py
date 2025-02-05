eb118a993fc09743b0c86110f86f5a2ba1ca921e
HoshiyarDigari <hoshiyarsingh@gmail.com>
2025-02-05 12:38:24 -0500

    now need to add feature to not start traversal from the first element , which has been visited, we need to skip the visited and removed elements after first pass

--- predictPartyVictory.py
+++ predictPartyVictory.py
@@ -1,8 +1,28 @@
+from collections import deque
 def predictPartyVictory(senate: str) -> str:
-    pass
+    queue = deque(senate)
+    complement = {'R':'D', 'D':'R'}
+    counter = 0
+    visited = set()
+    removed = set()
+    while queue and counter < 7:
+        counter +=1
+      
+        for senator in queue:
+            if  queue.index(senator) not in visited:
+                if complement[senator] in queue and queue.index(complement[senator]) not in removed :
+                    #queue.remove(complement[senator])
+                    visited.add(queue.index(senator))
+                    removed.add(queue.index(complement[senator]))
+                    print(queue, visited,removed)
+                    break
+                else:
+                    print('winner is ', queue[0])
+                    return queue[0]
+    
 
 
 
 if __name__ == "__main__":
-    senate = "RD"
+    senate = "RDRDDDR"
     predictPartyVictory(senate)
