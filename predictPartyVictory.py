0ae4455726cad2307259cce31990fefad73b341e
HoshiyarDigari <hoshiyarsingh@gmail.com>
2025-02-05 12:40:12 -0500

    add feature to keep count of visited senators

--- predictPartyVictory.py
+++ predictPartyVictory.py
@@ -3,22 +3,16 @@ def predictPartyVictory(senate: str) -> str:
     queue = deque(senate)
     complement = {'R':'D', 'D':'R'}
     counter = 0
-    visited = set()
-    removed = set()
     while queue and counter < 7:
         counter +=1
-      
         for senator in queue:
-            if  queue.index(senator) not in visited:
-                if complement[senator] in queue and queue.index(complement[senator]) not in removed :
-                    #queue.remove(complement[senator])
-                    visited.add(queue.index(senator))
-                    removed.add(queue.index(complement[senator]))
-                    print(queue, visited,removed)
-                    break
-                else:
-                    print('winner is ', queue[0])
-                    return queue[0]
+            if complement[senator] in queue:
+                queue.remove(complement[senator])
+                print(queue)
+                break
+            else:
+                print('winner is ', queue[0])
+                return queue[0]
     
 
 
