91463e8738bc69a7729f8915892cf71ba3d70501
HoshiyarDigari <hoshiyarsingh@gmail.com>
2025-02-06 13:50:25 -0500

    start

---
+++ deleteMiddle.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    pass
+        
+
+
+if __name__ == "__main__":
+    head = [1,3,4,7,1,2,6]
+    deleteMiddle(head)
