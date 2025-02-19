dc70f1b5cfac48a689150a4e369b85a3dfc2e6dd
HoshiyarDigari <hoshiyarsingh@gmail.com>
2025-02-19 14:24:24 -0500

    start

---
+++ maxLevelSum.py
@@ -0,0 +1,70 @@
+from typing import Optional
+from collections import deque
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+    
+    
+    #repr method for representing the node
+    def __str__(self):
+        result=''
+        str_left = str(self.left) 
+        str_right = str(self.right)
+        result+='TreeNode{val:'+str(self.val) +',Left:'+str_left + ',Right:'+ str_right
+        return result
+
+    
+    # equals function
+    def __eq__(self,other):
+        if not isinstance(other, TreeNode):
+            return False # other object is not even the same type of object
+        return id(self) == id(other) 
+    # we have a hash function too 
+    def __hash__(self):
+        return hash(self.val, self.right, self.left)
+
+def list_to_tree(lst):
+    if not lst:
+        return None
+    
+    root = TreeNode(lst[0])
+    queue = deque([root])
+    i=1
+    while queue and i < len(lst):
+        node = queue.popleft()
+        if lst[i] is not None and i < len(lst):
+            node.left = TreeNode(lst[i])
+            queue.append(node.left)
+            i+=1
+        else:
+            i+=1
+        if i < len(lst):
+            if lst[i] is not None:
+                node.right = TreeNode(lst[i])
+                queue.append(node.right)
+                i+=1
+            else:
+                i+=1
+    # print(root)
+    return root 
+
+    
+
+class Solution:
+    def maxLevelSum(self,root: Optional[TreeNode]) -> int:
+        """
+        Goal:
+        Find the level with nodes that have the max sum among all levels
+        """
+        
+        
+        
+if __name__ == "__main__":
+    root = [1,7,0,7,-8,None,None]
+    answer = Solution()
+    answer.maxLevelSum(list_to_tree(root))
+
+         
