56d9812f5165cd5b298df99d9a4326b396f0e338
HoshiyarDigari <hoshiyarsingh@gmail.com>
2025-03-02 01:15:13 -0500

    removed the distance dictionary, instead using the queue itself for keeping distanc of each node, improved runtime slightly

--- nearestExit.py
+++ nearestExit.py
@@ -9,18 +9,19 @@ class Solution:
         """
         maze_boundary = (len(maze),len(maze[0]))
         queue = deque()
-        queue.append(entrance)
+        queue.append([entrance,0])
+        print(queue)
         visited = {tuple(entrance)} # entrance is a list, we convert to tuple so we can use in a set
-        distance = {tuple(entrance):0} # dictionary to hold distance of each node from entrance
+        
         while queue:
-            current_cell = queue.popleft()
+            current_cell, current_distance = queue.popleft()
             print('looking at ', current_cell, 'and visited', visited)
             #check if this is an exit
             current_row, current_column = current_cell[0], current_cell[1]
             # exits are when either row or column equal 0 or max value, we can't exit at entrance
             if (current_cell!=entrance) and ((current_row == maze_boundary[0]-1 or current_row==0) or (current_column==0 or current_column==maze_boundary[1]-1)):
-                print('exit is at ',current_cell , 'with distance of ', distance[tuple(current_cell)])
-                return distance[tuple(current_cell)]
+                print('exit is at ',current_cell , 'with distance of ', current_distance)
+                return current_distance
             # get the 4 possible neighbors up, down, left and right
             neighbors = [(current_row-1,current_column), (current_row+1,current_column),(current_row,current_column-1),(current_row,current_column+1)]
             for neighbor in neighbors:
@@ -28,8 +29,8 @@ class Solution:
                 if (neighbor not in visited) and (neighbor[0] in range(maze_boundary[0])) and (neighbor[1] in range(maze_boundary[1])):
                     if (maze[neighbor[0]][neighbor[1]]!="+"):
                             visited.add(neighbor)
-                            queue.append(list(neighbor))
-                            distance[neighbor] = distance[tuple(current_cell)] + 1
+                            queue.append([neighbor,current_distance+1])
+                            
         return -1
 
 
