1429f3237ebc8a43ff915e23e7dc6c5657b8d82e
HoshiyarDigari <hoshiyarsingh@gmail.com>
2025-03-02 01:05:22 -0500

    solved with 13% score

--- nearestExit.py
+++ nearestExit.py
@@ -7,32 +7,47 @@ class Solution:
         - Use BFS as we want to find the shortest exit.
         
         """
-        maze_rows = len(maze)
-        maze_columns = len(maze[0])
+        maze_boundary = (len(maze),len(maze[0]))
         queue = deque()
         queue.append(entrance)
-        print(queue, maze_rows , 'x', maze_columns)
-        visited = {tuple(entrance)}
+        visited = {tuple(entrance)} # entrance is a list, we convert to tuple so we can use in a set
+        distance = {tuple(entrance):0} # dictionary to hold distance of each node from entrance
         while queue:
             current_cell = queue.popleft()
             print('looking at ', current_cell, 'and visited', visited)
             #check if this is an exit
-            row, column = current_cell[0], current_cell[1]
-            if (row == maze_rows-1 or row==0) or (column==0 or column==maze_columns-1):
-                print('exit is at ',current_cell)
-                break
+            current_row, current_column = current_cell[0], current_cell[1]
+            # exits are when either row or column equal 0 or max value, we can't exit at entrance
+            if (current_cell!=entrance) and ((current_row == maze_boundary[0]-1 or current_row==0) or (current_column==0 or current_column==maze_boundary[1]-1)):
+                print('exit is at ',current_cell , 'with distance of ', distance[tuple(current_cell)])
+                return distance[tuple(current_cell)]
             # get the 4 possible neighbors up, down, left and right
-            neighbors = [(row-1,column), (row+1,column),(row,column-1),(row,column+1)]
+            neighbors = [(current_row-1,current_column), (current_row+1,current_column),(current_row,current_column-1),(current_row,current_column+1)]
             for neighbor in neighbors:
-                if (neighbor not in visited) and (maze[neighbor[0]][neighbor[1]]!="+") and (neighbor[0]<maze_rows) and (neighbor[1] < maze_columns):
+                # check that this neighbor is not yet visited, is not a wall and is within the maze
+                if (neighbor not in visited) and (neighbor[0] in range(maze_boundary[0])) and (neighbor[1] in range(maze_boundary[1])):
+                    if (maze[neighbor[0]][neighbor[1]]!="+"):
                             visited.add(neighbor)
                             queue.append(list(neighbor))
-
+                            distance[neighbor] = distance[tuple(current_cell)] + 1
+        return -1
 
 
 if __name__ == "__main__":
     solution=Solution()
+
+    #test 1
     maze = [["+","+",".","+"],[".",".",".","+"],["+","+","+","."]]
     entrance = [1,2]
     result = solution.nearestExit(maze, entrance)
     assert result == 1
+
+    #test2
+    maze = [["+","+","+"],[".",".","."],["+","+","+"]]
+    entrance = [1,0]
+    assert solution.nearestExit(maze,entrance)== 2
+
+    #test 3
+    maze = [[".","+"]]
+    entrance = [0,0]
+    assert solution.nearestExit(maze,entrance) == -1
