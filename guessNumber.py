2669ef3c6313efdd504b0add5e9bb36e0480de0c
HoshiyarDigari <hoshiyarsingh@gmail.com>
2025-03-17 11:59:27 -0400

    solved - in binary search we have to reset the mid point with a plus one

--- guessNumber.py
+++ guessNumber.py
@@ -3,8 +3,36 @@
 # @return -1 if num is higher than the picked number
 #          1 if num is lower than the picked number
 #          otherwise return 0
-# def guess(num: int) -> int:
+def guess(num: int) -> int:
+    pick=7
+    if num == pick:
+        return 0
+    if num < pick:
+        return 1
+    return -1
 
 class Solution:
-    def guessNumber(self, n: int) -> int:
-        pass
+    def guessNumber( n: int) -> int:
+        """
+        Algo:
+        implement basic binary search from 1 to n
+        """
+        if guess(n)== 0:
+            return n
+        start, end = 1,n
+        while(True):
+            my_guess = (start + end)//2
+            result = guess(my_guess)
+            if result == 0:
+                return my_guess 
+            elif result == 1: #guessed too low so bring up the start point
+                start = my_guess + 1 # important to set one more than guess to avoid infinite loops and better runtimes, same with end
+            else:
+                end = my_guess -1 
+
+
+assert Solution.guessNumber(10)== 6
+
+
+
+
