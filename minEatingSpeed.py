73729a32626a1a9e1e77dfc2d41d8eaf3c5bbad5
HoshiyarDigari <hoshiyarsingh@gmail.com>
2025-03-20 19:57:33 -0400

    solved- main idea is to do binary search from 1 to the max number of bananas in a pile, if it takes more time to eat than given hours, its eating too slow , move up the start number else its eating too fast, move down the end, we might also have situation where there is no number to finish in exactly h hours so keep a note of all numbers that lead to eating bananas within the h hours

--- minEatingSpeed.py
+++ minEatingSpeed.py
@@ -1,8 +1,44 @@
-import time
+import time, math
 class Solution:
     
         def minEatingSpeed(piles: list[int], h: int) -> int:
-              pass
+            """
+            Algo:
+            1. sort the piles 
+            2. Koko can eat from 1 to the max number in the pile per hour
+            3. if we have same number of piles as number of hours, she has to eat the maximum number to finish all piles
+            4. if not, we have to find the minimum number that will allow her to eat all of them in the h hours. 
+            5. we do a binary search in the search space of 1 to max to find this number.
+            """
+            eating_speed = float('inf') # we will respond with lowest possible rate of eating
+            total_piles = len(piles)
+            #sort piles
+            piles.sort()
+            # quick exit 
+            if h == total_piles:
+                return piles[-1] #she has to eat the biggest pile in one go to finish in time
+            #binary search
+            start = 1
+            end = piles[-1]
+            while (start<=end or eating_speed == 'inf'):
+                mid = (start+end)//2
+                time_to_finish = 0
+                for bananas in piles:
+                    time_to_finish= time_to_finish + math.ceil(bananas/mid)
+                    if time_to_finish>h:
+                        break
+                    
+                if time_to_finish <= h:
+                    eating_speed = min(eating_speed, mid)
+                    #check if we can get a slower eating rate
+                    end = mid - 1
+                else: # ate too slow
+                     start = mid + 1 
+                
+            print(eating_speed)
+            return eating_speed
+            
+
 
             
 
@@ -10,9 +46,9 @@ class Solution:
 if __name__ == '__main__':
     #test1
     start_time = time.perf_counter()
-    piles = [3,6,7,11]
-    h = 8
-    assert Solution.minEatingSpeed(piles, h) in {4 }
+    piles = [4]
+    h = 3
+    assert Solution.minEatingSpeed(piles, h) in {2 }
     end_time = time.perf_counter()
     exec_time = (end_time - start_time) * 1000
     print(f'function took {exec_time:.6f} miliseconds' )
