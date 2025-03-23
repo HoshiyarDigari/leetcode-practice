b953fea574788e05d22f21774348c9ad31c5384d
HoshiyarDigari <hoshiyarsingh@gmail.com>
2025-03-23 00:37:38 -0400

    solved with general template and made it effective

--- letterCombinations.py
+++ letterCombinations.py
@@ -20,47 +20,23 @@ class Solution:
             return []
         digitMap = {'2':('a', 'b', 'c'), '3':('d','e','f'), '4':('g','h','i'), '5':('j','k','l'), '6':('m','n','o'), '7':('p', 'q','r','s'), '8':('t','u','v'), '9':('w', 'x','y','z')}
         
-        choices = [ y for x in digits for  y in digitMap[x] ]
+        def backtrack(state, combinations, index):
+            #if index exceeds the digits array, we have reached the depth of the tree and need to back track, this is the step that stops the recursion
+            if index== len(digits):
+                combinations.append(''.join(state))
+                return
+            choices = [ x for x in digitMap[digits[index]]]
+            for choice in choices:
+                state.append(choice) #make choice
+                backtrack(state, combinations, index + 1)
+                #undo choice - backtrack
+                state.pop()
+                
+    
+        index = 0
         state = []
         combinations = []
-        
-        def is_valid_choice(choice, state):
-            #two pairs
-            if len(state)== 0:
-                return True # first choice is free
-            index = 0
-            for letter in state:
-                if letter not in digitMap[digits[index]]:
-                    return False
-                index+=1
-            #now check choice is from the next available digitmap
-            if choice not in digitMap[digits[index]]:
-                return False
-            return True
-
-
-        def make_choice(choice, state):
-            state.append(choice)
-        def undo_choice(choice, state):
-            state.pop()
-        def is_solution(state, combinations): 
-            if len(state)== len(digits) and ''.join(state[:]) not in combinations:
-                return True
-            return False
-        def backtrack(state, choices, combinations):
-            if is_solution(state, combinations):
-                combinations.append(''.join(state[:]))
-                print('combination got a new succss state', combinations)
-                return # this is the base case that stops the recursion
-            #is solution doesn't allow for duplicates, so if we have reached the intended string length, we need to return without adding to combinations
-            if len(state) == len(digits):
-                return
-            for choice in choices:
-                if is_valid_choice(choice, state):
-                    make_choice(choice, state)
-                    backtrack(state, choices, combinations)
-                    undo_choice(choice, state)
-        backtrack(state, choices, combinations)
+        backtrack(state,combinations, index)
         print(combinations)
         return combinations
 
