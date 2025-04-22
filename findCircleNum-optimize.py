7a06a7dd34507d0f8faaf9990030bf93acbce483
HoshiyarDigari <hoshiyarsingh@gmail.com>
2025-04-22 11:12:03 -0400

    solved with DFS

---
+++ findCircleNum-optimize.py
@@ -0,0 +1,106 @@
+from typing import Optional
+from collections import deque
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+    
+    
+    #repr method for representing the node
+    def __str__(self):
+        result=''
+        str_left = str(self.left) 
+        str_right = str(self.right)
+        result+='TreeNode{val:'+str(self.val) +',Left:'+str_left + ',Right:'+ str_right
+        return result
+
+    
+    # equals function
+    def __eq__(self,other):
+        if not isinstance(other, TreeNode):
+            return False # other object is not even the same type of object
+        return id(self) == id(other) 
+    # we have a hash function too 
+    def __hash__(self):
+        return hash(self.val, self.right, self.left)
+
+def list_to_tree(lst):
+    if not lst:
+        return None
+    
+    root = TreeNode(lst[0])
+    queue = deque([root])
+    i=1
+    while queue and i < len(lst):
+        node = queue.popleft()
+        if lst[i] is not None and i < len(lst):
+            node.left = TreeNode(lst[i])
+            queue.append(node.left)
+            i+=1
+        else:
+            i+=1
+        if i < len(lst):
+            if lst[i] is not None:
+                node.right = TreeNode(lst[i])
+                queue.append(node.right)
+                i+=1
+            else:
+                i+=1
+    # print(root)
+    return root 
+
+    
+
+class Solution:
+    def findCircleNum(self, isConnected: list[list[int]]) -> int:
+        """
+        Algo:
+        we want to do a depth first approach. We start at one city and explore all its connections and their connnections.
+        we add the cities to a visited list to keep track  and we stop when we have no more cities to visit 
+        """
+        visited= set() #track the visited cities
+        
+        def visit_connections(current, isConnected, visited, total_cities):
+            visited.add(current)
+            for other_city in range(len(isConnected)):
+                if isConnected[current][other_city] and other_city not in visited:
+                    visit_connections(other_city,isConnected,visited,total_cities)
+            return 
+
+        provinces = 0
+        #start exploration
+        total_cities= len(isConnected[0])
+        for city in range(total_cities):
+            if city not in visited:
+                visit_connections(city, isConnected, visited, total_cities)
+                provinces+=1
+        return provinces
+                
+        
+        
+        
+if __name__ == "__main__":
+    isConnected = [
+    [1,1,0,0,0,0,0,1,0,0,0,0,0,0,0],
+    [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,1,0,1,1,0,0,0,0,0,0,0,0],
+    [0,0,0,0,1,0,0,0,0,1,1,0,0,0,0],
+    [0,0,0,1,0,1,0,0,0,0,1,0,0,0,0],
+    [0,0,0,1,0,0,1,0,1,0,0,0,0,1,0],
+    [1,0,0,0,0,0,0,1,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,1,1,1,0,0,0,0,1,0],
+    [0,0,0,0,1,0,0,0,0,1,0,1,0,0,1],
+    [0,0,0,0,1,1,0,0,0,0,1,1,0,0,0],
+    [0,0,0,0,0,0,0,0,0,1,1,1,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0],
+    [0,0,0,0,0,0,1,0,1,0,0,0,0,1,0],
+    [0,0,0,0,0,0,0,0,0,1,0,0,0,0,1]]
+
+
+    answer = Solution()
+    answer.findCircleNum(isConnected)
+
+         
